4d45609059469443bf87b960e4b55945edde5302
kiasamouie <thekiadoe@gmail.com>
2023-10-03 18:54:18 +0100

    inital commit

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Samui Productions</title>
+	<meta name="description" content="Samui Productions">
+	<meta name="author" content="Samui Productions">
+	<meta name="keywords" content="music production, beats, mixing, mastering, music studio, portfolio, soundcloud, spotify, apple music, youtube, contact,social media, collaborations, projects, genre, tracks, artist, bio, testimonials, store, events">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="icon" type="image/png" href="img/icons/logo.png" />
+	<link rel="stylesheet" href="css/slit-slider.css">
+	<link rel="stylesheet" href="css/main.css">
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="https://kit.fontawesome.com/6bee25835f.js" crossorigin="anonymous"></script>
+</head>
+
+<body id="body">
+
+	<header id="navigation" class="navbar-inverse navbar-fixed-top animated-header">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="header-brand">
+					<img src="img/icons/logo.png" alt="logo" width="30" height="30"/>
+					<h1 class="navbar-brand">Samui Productions</h1>
+				</div>
+			</div>
+			<nav class="collapse navbar-collapse navbar-right" role="navigation">
+				<ul id="nav" class="nav navbar-nav">
+					<li><a href="#body">Home</a></li>
+					<li><a href="#price">Price</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<?php 
+		function shuffle_assoc($list) { 
+			$keys = array_keys($list); 
+			shuffle($keys); 
+			$random = [];
+			foreach ($keys as $key) $random[$key] = $list[$key];
+			return $random; 
+		}
+		$socials = json_decode(file_get_contents("socials.json"), true);
+		$order = array_combine(range(1, count($socials)), array_values(array_keys($socials)));
+		$socials = shuffle_assoc($socials);
+	?>
+	<main class="site-content" role="main">
+		<section id="home-slider">
+			<div id="slider" class="sl-slider-wrapper">
+				<?php foreach ($socials as $name => $data) { ?>
+					<div class="sl-slider">
+						<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+							<div class="bg-img bg-img-<?php echo array_search($name, $order)?>"></div>
+							<div class="slide-caption">
+								<div class="caption-content">
+									<div class="caption-box">
+										<h2 class="animated fadeInDown"><?php echo $name ?></h2>
+										<?php foreach ($data as $social => $url) { ?>
+											<a href="<?php echo $url ?>" target="_blank">
+												<i class="fab fa-<?php echo $social ?>">&nbsp;</i> <?php echo ucfirst($social) ?>
+											</a>
+										<?php } ?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+				<nav id="nav-arrows" class="nav-arrows visible-xs visible-sm visible-md visible-lg">
+					<a href="javascript:;" class="sl-prev"><i class="fa fa-angle-left fa-3x"></i></a>
+					<a href="javascript:;" class="sl-next"><i class="fa fa-angle-right fa-3x"></i></a>
+				</nav>
+			</div>
+		</section>
+	</main>
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.singlePageNav.min.js"></script>
+	<script src="js/jquery.slitslider.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.ba-cond.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
